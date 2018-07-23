ba855ed62658e4eac11602bfc026d1e21be78965
Jarek <z.jarek92@gmail.com>
2018-07-23 13:46:00 +0200

    mapping in react

--- src/WhartReactRender.js
+++ src/WhartReactRender.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 const string = 'Ala ma kota!'
+const number = 123
+const bool = true
+const undefinedVar = undefined
+const nulVar = null
+const emtyArray = []
+const arrWithStrings = ['Ala', 'Ela', 'Ilona']
 
 const WhartReactRender = () => (
     <div>
         {string}
-  </div>
+        <br />
+        {number}
+        <br />
+        {bool}
+        <br />
+        {undefined}
+        <br />
+        {null}
+        <br />
+        {emtyArray}
+        <br />
+        {arrWithStrings}
+        <br />
+        {arrWithStrings[0]}{arrWithStrings[1]}{arrWithStrings[2]}
+
+        <ul>
+            {
+                arrWithStrings.map(string => <li>{string}</li>)
+            }
+        </ul>
+    </div>
 )
 
 export default WhartReactRender
