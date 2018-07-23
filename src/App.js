15bffbe9b10f85e7a138f90dfb0d0f039b8d0961
Jarek <z.jarek92@gmail.com>
2018-07-23 11:48:00 +0200

    Canging component into function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
+
+
+const App = () => (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,8 +14,6 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
-  }
-}
+    )
 
-export default App;
+export default App
