f7193a5553f888474470d7bb03af573a1e8791e3
Jarek <z.jarek92@gmail.com>
2018-07-23 12:16:31 +0200

    emmbeding expressions

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const appClassPrefix = 'App'
 
 
 const App = () => (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+  <div className={`${appClassPrefix}`}>
+    <header className={`${appClassPrefix}-header`}>
+      <img src={logo} className={`${appClassPrefix}-logo`} alt={'logo'} />
+      <h1 className={`${appClassPrefix}-title`}>Welcome to React</h1>
+    </header>
+    <p className={`${appClassPrefix}-intro`}>
+      To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    )
+  </div>
+)
 
 export default App
