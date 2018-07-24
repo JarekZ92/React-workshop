0b28554bf58771fdf9ce1e59a6b922bedaffded7
Jarek <z.jarek92@gmail.com>
2018-07-24 11:03:39 +0200

    SingingButtonWithBorder added propos to SingButton

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
-import SingingButton from './SingingButton';
-import Border from './Border'
+import SingingButtonWithBorder from './SingingButtonWithBorder'
 
 const App = () => (
-  <Border>
-    <SingingButton
+  <div>
+    <SingingButtonWithBorder
       label={'Przycisk 1'}
       sound={'La la la'}
       makeSound={() => alert('Im from makeSound')}
     />
-    <SingingButton
-      label={'Przycisk 2'}
-      sound={'Kotki dwa!'}
-    />
-  </Border>
+  </div>
 
 )
 
---
+++ src/SingingButtonWithBorder.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Border from './Border'
+import SingingButton from './SingingButton'
+
+
+const SingingButtonWithBorder = (props) => (
+    <Border>
+        <SingingButton
+            label={props.label}
+            sound={props.sound}
+            makeSound={props.makeSound}
+        />
+    </Border>
+)
+
+export default SingingButtonWithBorder
