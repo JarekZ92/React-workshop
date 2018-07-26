0d13c31a153604caf457c4bc71a6c977f1ca18ca
Jarek <z.jarek92@gmail.com>
2018-07-26 11:49:10 +0200

    Fatching data in  componentDidMount

--- src/Views/FetchingRandomUsers/FetchingRandomUsers.js
+++ src/Views/FetchingRandomUsers/FetchingRandomUsers.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 class FetchingRandomUsers extends React.Component {
+    state = {
+        randomUserData: null
+    }
+
+    componentDidMount() {
+        fetch('https://randomuser.me/api')
+            .then(response => response.json())
+            .then(dataFromResponse => {
+                this.setState({
+                    randomUserData: dataFromResponse
+                })
+            })
+    }
+
     render() {
         return (
             <div>
-                FetchingRandomUsers
+                {JSON.stringify(this.state.randomUserData)}
             </div>
         )
     }
